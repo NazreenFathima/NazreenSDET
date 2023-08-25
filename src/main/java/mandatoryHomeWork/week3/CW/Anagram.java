834c897a80ef0f76d499aa5676b531586557fb1f
NazreenFathima <nazreenfathimaraja@gmail.com>
2023-08-25 11:11:55 +0530

    Push to correct folder

---
+++ src/main/java/mandatoryHomeWork/week3/CW/Anagram.java
@@ -0,0 +1,52 @@
+package mandatoryHomeWork.week3.CW;
+
+import java.util.Arrays;
+
+import org.junit.Test;
+
+import junit.framework.Assert;
+
+public class Anagram 
+{
+	/*
+	 * Input : String
+	 * Output : Boolean
+	 * 
+	 * Constraints : No DS
+	 * 
+	 * Psedocode : 
+	 * 1) Convert the string to char[]
+	 * 2) Sort the char[]
+	 * 3) If both char[] are equal the anagram else its not anagram
+	 * 
+	 */
+	
+	public boolean isAnagram (String s, String t)
+	{
+		char[] schar = s.toCharArray();
+		char[] tchar = t.toCharArray();
+		Arrays.sort(schar);
+		Arrays.sort(tchar);
+		
+		return Arrays.equals(schar, tchar );
+		
+	}
+	
+	@Test
+	public void test1()
+	{
+		boolean actual = isAnagram ("anagram", "nagaram");
+		Assert.assertEquals(true, actual);
+		
+	}
+	
+	@Test
+	public void test2()
+	{
+		boolean actual = isAnagram ("rat", "car");
+		Assert.assertEquals(false, actual);
+		
+	}
+	
+	
+}
---
+++ src/main/java/mandatoryHomeWork/week3/CW/ExcelSheetColumnNumber.java
@@ -0,0 +1,57 @@
+package mandatoryHomeWork.week3.CW;
+
+import org.junit.Test;
+
+import junit.framework.Assert;
+
+public class ExcelSheetColumnNumber 
+{
+	/*
+	 * Input : String
+	 * Output : int
+	 * 
+	 * Constraints : No DS
+	 * 
+	 * Pseudocode : 
+	 * 1)
+	 * 2)
+	 * 3)
+	 * 4)
+	 *
+	 */
+	
+	public int columnNumber (String columnTitle)
+	{	
+		if(columnTitle.length()==1)
+		{
+		return (1)+columnTitle.charAt(0)-'A';
+		}
+		int result=0;
+		for(int i=0;i<columnTitle.length();i++)
+		{
+		result=result*26+(columnTitle.charAt(i)-'A'+1);
+		}
+		return result;
+	}
+	
+	@Test
+	public void test1()
+	{
+		int actual = columnNumber ("AB");
+		Assert.assertEquals(28, actual);
+	}
+	
+	@Test
+	public void test2()
+	{
+		int actual = columnNumber ("A");
+		Assert.assertEquals(1, actual);
+	}
+	
+	@Test
+	public void test3()
+	{
+		int actual = columnNumber ("ZY");
+		Assert.assertEquals(701, actual);
+	}
+}
