a08d04c06eb35d197e24429b3ec503127760d30e
NazreenFathima <88609101+NazreenFathima@users.noreply.github.com>
2023-09-02 13:12:22 +0530

    updated code

--- src/main/java/mandatoryHomeWork/selenium/salesforce/TC_01.java
+++ src/main/java/mandatoryHomeWork/selenium/salesforce/TC_01.java
@@ -7,6 +7,9 @@ import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 
 public class TC_01 {
 
@@ -23,6 +26,8 @@ public class TC_01 {
 			 * 5. Click on New Individual
 			 * 6. Enter the Last Name as 'Kumar'
 			 * 7.Click save and verify Individuals Name"
+			 * 
+			 * DOM Freeze : setTimeout(function() {debugger;}, 3000);
 			 */		
 			ChromeOptions options = new ChromeOptions();		
 			options.addArguments("--disable-notifications");				
@@ -40,29 +45,24 @@ public class TC_01 {
 			
 			//3. Click View All and click Individuals from App Launcher		
 			driver.findElement(By.xpath("//button[@class='slds-button']")).click();			
-			//driver.findElement(By.xpath("//p[text()='Individuals']")).click();				
-			//JavascriptExecutor jsExecutor = (JavascriptExecutor) driver;		
-			//WebElement individual = driver.findElement(By.xpath("//p[text()='Individuals']"));		
-			//jsExecutor.executeScript("arguments[0].scrollIntoView(true);", individual);		
-			//individual.click();		driver.findElement(By.xpath("//input[@part='input']")).sendKeys("individual");				
+					
+			driver.findElement(By.xpath("(//input[@type='search'])[3]")).sendKeys("Individuals");
 			JavascriptExecutor jsExecutor = (JavascriptExecutor) driver;		
-			WebElement individual = driver.findElement(By.xpath("//span[@class='slds-truncate label-display']"));	 
+			WebElement individual = driver.findElement(By.xpath("//p[@class='slds-truncate']"));	 
 			jsExecutor.executeScript("arguments[0].click();", individual);			 
-			Thread.sleep(1000);		
+			Thread.sleep(1000);	
+			
 			//5. Click on New		
-			driver.findElement(By.xpath("//div[text()='New']")).click();				
+			driver.findElement(By.xpath("//div[text()='New']")).click();
+			
 			//6. Enter the Last Name as 'Kumar'		
 			driver.findElement(By.xpath("//input[@placeholder='Last Name']")).sendKeys("Kumar");						
 			driver.findElement(By.xpath("(//span[text()='Save'])[2]")).click();
-			String lastName = driver.findElement(By.xpath("//div[@class='slds-page-header__title slds-m-right--small slds-align-middle clip-text slds-line-clamp']")).getText();		
-			if(lastName.equals("Kumar"))		
-			{			
-				System.out.println("Individuals Name verified");	
-			}		
-			else 		
-			{			
-				System.out.println("Individuals Name not verified");
-			}		
+			WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+			wait.until(ExpectedConditions.visibilityOf(driver.findElement(By.xpath("//span[@class='toastMessage slds-text-heading--small forceActionsText']"))));
+			Assert.assertEquals(true, driver.findElement(By.xpath("//span[@class='toastMessage slds-text-heading--small forceActionsText']")).getText().contains("Kumar"));
+			driver.quit();
+				
 		}
 
 }
