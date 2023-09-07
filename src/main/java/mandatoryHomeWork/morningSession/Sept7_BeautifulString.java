16c8dff080d4396296ce27338709a702437da5b8
NazreenFathima <88609101+NazreenFathima@users.noreply.github.com>
2023-09-07 07:42:12 +0530

    BeautifulString

---
+++ src/main/java/mandatoryHomeWork/morningSession/Sept7_BeautifulString.java
@@ -0,0 +1,61 @@
+package mandatoryHomeWork.morningSession;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class Sept7_BeautifulString 
+{
+	/*
+	 * understood : yes
+	 * input : String
+	 * output : int
+	 * 
+	 * Pseudocode:
+	 * 1)Initialise count=0;
+	 * 2)Iterate through for loop
+	 * 3)if even--> expectedChar =0; else 1
+	 * 4)ChatAt(i)!=expectedChar -> count++
+	 */
+	
+	public int beautifulString(String str)
+	{
+		int count =0;
+		for (int i=0; i<str.length(); i++)
+		{
+			char expectedChar;
+			if (i % 2 == 0) 
+			{
+                expectedChar = '0';
+            } else {
+                expectedChar = '1';
+            }
+
+            if (str.charAt(i) != expectedChar) 
+            {
+            	count++;
+            }
+		}
+		return count;
+	}
+	
+	 @Test
+	 public void test1() 
+	 {
+	 int actual = beautifulString("0101");
+	 Assert.assertEquals(0, actual);
+	 }
+	 
+	 @Test
+	 public void test2() 
+	 {
+	 int actual = beautifulString("0000");
+	 Assert.assertEquals(2, actual);
+	 }
+	 
+	 @Test
+	 public void test3() 
+	 {
+	 int actual = beautifulString("01011");
+	 Assert.assertEquals(1, actual);
+	 }
+}
