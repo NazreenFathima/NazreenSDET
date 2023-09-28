46fd16055457a33ea7a17d9e0bf014fb75048234
NazreenFathima <88609101+NazreenFathima@users.noreply.github.com>
2023-09-28 23:04:20 +0530

    FindAnagram

--- src/main/java/mandatoryHomeWork/week11/HW/FindAnagrams_BruteForce.java
+++ src/main/java/mandatoryHomeWork/week11/HW/FindAnagrams_BruteForce.java
@@ -15,6 +15,15 @@ public class FindAnagrams_BruteForce
 	 * output : List<Integer>
 	 * 
 	 * Pseudocode:
+	 * 1) Convert String p to char[]
+	 * 2) Sort the p char[]
+	 * 3) Create a ArrayList
+	 * 4) Iterate through the for loop (less than nd equal to -- String s minus String p length)
+	 * 5) get the substring of p length
+	 * 6) convert to char[] and sort
+	 * 7) compare both the char[] --if equal add to arrayLsit
+	 * 8) return the arrayLsit
+	 * 
 	 * 
 	 */
 	
